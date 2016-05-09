09a2c431731b77490a234e3fd3b4596b994fda4b
David Beniamine <david@beniamine.net>
2016-05-09 10:57:04 +0200

    settings for libinput

--- xinput_mouse_settings.sh
+++ xinput_mouse_settings.sh
@@ -1,52 +1,56 @@
 #!/bin/bash
 
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#                    Version 2, December 2004
-#
-# Copyright (C) 2015 Beniamine, David <David@Beniamine.net>
+# Copyright (C) 2016  Beniamine, David <David@Beniamine.net>
 # Author: Beniamine, David <David@Beniamine.net>
 #
-# Everyone is permitted to copy and distribute verbatim or modified
-# copies of this license document, and changing it is allowed as long
-# as the name is changed.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#  0. You just DO WHAT THE FUCK YOU WANT TO.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Ugly settings for mouse input, must be called each time a mouse is added
-# (using udev rules for instance)
 
-reverse_scrolling()
+# Ugly settings for mouse input, using libinput if avaialable or
+# Synpatics/Evdev
+
+ugly_reverse_scrolling()
 {
     list=$(xinput get-button-map $1)
     list=${list/ 4 5/ 5 4 }
     xinput set-button-map $1 $list
 }
 
-#Use Wheel emulation for all mouse with button 3
-xinput --list | grep "id=" | cut -d "=" -f 2 | sed -e 's/^\([0-9]*\).*$/\1/'\
-    | while read id
+xinput --list | grep "slave.*pointer" | grep -v XTEST | \
+    sed -e 's/.*id=\([0-9]*\).*/\1/' | while read id
 do
-    xinput list-props $id | grep "Evdev Wheel Emulation"
-    if [ $? -eq 0 ]
+    device=$(xinput list-props $id | head -n 1 | sed -e "s/.*'\(.*\)'/\1/")
+    echo "Seting input $device $id"
+    if [ ! -z "$(xinput list-props $id | grep libinput)" ]
     then
+        echo "Using libinput"
+        xinput --set-prop $id "libinput Tapping Enabled" 1
+        xinput --set-prop $id "libinput Middle Emulation Enabled" 1
+        xinput --set-prop $id "libinput Natural Scrolling Enabled" 1
+        xinput --set-prop $id "libinput Button Scrolling Button" 3
+    else
+        echo "Using synaptics / Evdev"
         xinput --set-prop $id "Evdev Wheel Emulation" 1
         xinput --set-prop $id "Evdev Wheel Emulation Button" 3
         xinput --set-prop $id "Evdev Wheel Emulation Axes" 6 7 4 5
-        reverse_scrolling $id
+        # pad corners rt rb lt lb notinh fingers 1 2
+        # 0: disable, 1 left, 2 middle, 3 right
+        xinput --set-prop $id "Synaptics Tap Action" 0 0 0 0 1 2
+        # Allow scrolling with two finger vertically and horizontally
+        xinput --set-prop $id "Synaptics Two-Finger Scrolling" 1 1
+        xinput --set-prop $id "Synaptics Palm Detection" 1
+        xinput --set-prop $id "Synaptics Palm Dimensions" 10 0
+        ugly_reverse_scrolling $id
     fi
 done
-
-#Touchpad settings
-id=$(xinput list | grep TouchPad | cut -d "=" -f 2 \
-    | sed -e 's/^\([0-9]*\).*$/\1/')
-# pad corners rt rb lt lb notinh fingers 1 2
-# 0: disable, 1 left, 2 middle, 3 right
-xinput --set-prop $id "Synaptics Tap Action" 0 0 0 0 1 2
-# Allow scrolling with two finger vertically and horizontally
-xinput --set-prop $id "Synaptics Two-Finger Scrolling" 1 1
-xinput --set-prop $id "Synaptics Palm Detection" 1
-xinput --set-prop $id "Synaptics Palm Dimensions" 10 0
-reverse_scrolling $id
